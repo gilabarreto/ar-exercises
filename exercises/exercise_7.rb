d90ec646434762f61327574fecb7b52e7d834d1e
Victor Gila Barreto <gilabarreto@gmail.com>
2022-10-23 21:26:03 -0600

    Check for errors.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -1,12 +1,16 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
-require_relative './exercise_5'
-require_relative './exercise_6'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
+require_relative "./exercise_5"
+require_relative "./exercise_6"
 
 puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter store name: "
+@store_name = gets.chomp
+
+puts Store.create(name: @store_name).errors.full_messages
